a29a93e4e2882151638f7d128eec180e4de5ab0d
Joyce Tao <jtao@lcs.on.ca>
2022-05-09 14:51:34 -0400

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by Joyce Tao on 2022-05-09.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
