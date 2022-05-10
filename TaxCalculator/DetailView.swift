b47add8a52be5d68cc092fae4538682c53bd63d7
Joyce Tao <jtao@lcs.on.ca>
2022-05-09 22:04:33 -0400

    Finished the static version of DetailView (created one sample list item)

--- TaxCalculator/DetailView.swift
+++ TaxCalculator/DetailView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            VStack(alignment: .leading) {
+                Text("2022/05/09")
+                    .bold()
+                Text("Original Price: $29.99")
+                    .font(.subheadline)
+                Text("Price with Tax: $33.89")
+                    .font(.subheadline)
+            }
+        }
+        .navigationTitle("History")
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        NavigationView {
+            DetailView()
+        }
     }
 }
