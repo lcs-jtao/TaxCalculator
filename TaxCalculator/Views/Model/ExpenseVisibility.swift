743bb0acb7aa9b1f2a89cf517f40134eaa723ff8
Joyce Tao <jtao@lcs.on.ca>
2022-11-02 22:28:15 -0400

    Used an enumeration that contains a defined list of possible values (categories); ExpenseVisibility is used to determine what expenses should be visible in the list of expenses in HistoryView

---
+++ TaxCalculator/Views/Model/ExpenseVisibility.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseVisibility.swift
+//  TaxCalculator
+//
+//  Created by Joyce Tao on 2022-11-02.
+//
+
+import Foundation
+
+enum ExpenseVisibility: String {
+    case all = "All"
+    case necessities = "Necessities"
+    case nonEssentials = "Non-Essentials"
+}
