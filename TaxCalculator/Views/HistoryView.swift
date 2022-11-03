62652c377d76deca5f9fd46512f771209a0b3724
Joyce Tao <jtao@lcs.on.ca>
2022-11-02 22:31:17 -0400

    Removed the original layout of displaying the list of expenses in each category; used a picker instead to selected a specific category, and created filtered function to filter the list of expenses by categories

--- TaxCalculator/Views/HistoryView.swift
+++ TaxCalculator/Views/HistoryView.swift
@@ -12,22 +12,52 @@ struct HistoryView: View {
     // MARK: Stored properties
     @Binding var expenses: [Expense]
     
+    // Tracks what results should be visible currently
+    @State private var selectedExpenseVisibility: ExpenseVisibility = .all
+    
     // MARK: Computed property
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(listOfCategories) { currentCategory in
-                    NavigationLink(destination: {
-                        ExpensesListView(expenses: $expenses, category: currentCategory.name)
-                    }, label: {
-                        HStack {
-                            Text(currentCategory.name)
-                            Image(systemName: currentCategory.image)
-                        }
-                    })
+        // Control filtering of prior tasks
+        VStack {
+            // Label for picker
+            Text("Filter by...")
+                .font(Font.caption.smallCaps())
+                .foregroundColor(.secondary)
+            
+            // Picker to allow user to select what tasks to show
+            Picker("Filter", selection: $selectedExpenseVisibility) {
+                Text(ExpenseVisibility.all.rawValue)
+                    .tag(ExpenseVisibility.all)
+                Text(ExpenseVisibility.necessities.rawValue)
+                    .tag(ExpenseVisibility.necessities)
+                Text(ExpenseVisibility.nonEssentials.rawValue)
+                    .tag(ExpenseVisibility.nonEssentials)
+            }
+            .pickerStyle(.segmented)
+            .padding(.horizontal)
+            
+            List(filtered(expenses, by: selectedExpenseVisibility)) { currentExpense in
+                DetailView(expense: currentExpense)
+            }
+        }
+    }
+    
+    
+    // MARK: Functions
+    // Filter the list of expenses to be shown
+    func filtered(_ expenses: [Expense], by visibility: ExpenseVisibility) -> [Expense] {
+        if visibility == .all {
+            return expenses
+        } else {
+            var filteredExpenses: [Expense] = []
+            for currentExpense in expenses {
+                if visibility == .necessities && currentExpense.category == "Necessities" {
+                    filteredExpenses.insert(currentExpense, at: 0)
+                } else if visibility == .nonEssentials && currentExpense.category == "Non-Essentials" {
+                    filteredExpenses.insert(currentExpense, at: 0)
                 }
             }
-            .navigationTitle("Expense History")
+            return filteredExpenses
         }
     }
 }
